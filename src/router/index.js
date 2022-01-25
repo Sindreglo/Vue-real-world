03725bbece0f5dc464fe6f1522cfe85b9050a1db
sindreglomnes <sindre.glomnes@gmail.com>
2022-01-25 19:48:40 +0100

    Update

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import EventList from "../views/EventList.vue";
+import EventDetails from "@/views/EventDetails";
+import About from "../views/About.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
